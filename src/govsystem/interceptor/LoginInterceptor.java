7e1c35620a2ad267f42c37975ab984d4c0f67208
caipengbo <caipengbo@outlook.com>
2017-06-10 23:59:43 +0800

    add interceptor

---
+++ src/govsystem/interceptor/LoginInterceptor.java
@@ -0,0 +1,41 @@
+package govsystem.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Title：登录拦截器
+ * Description：对于非登录的用户，拦截一些action
+ * Created by Myth on 6/10/2017.
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        HttpSession httpSession = httpServletRequest.getSession();
+        String roleName = (String)httpSession.getAttribute("roleName");
+        System.out.println("执行---------");
+        if ("user".equals(roleName)||"admin".equals(roleName)) {
+            return true;
+        }
+        System.out.println("执行完--");
+        String projectName = httpServletRequest.getContextPath();
+        httpServletResponse.sendRedirect(projectName + "/toFrontEndIndex.action");
+        return false;
+    }
+
+
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
